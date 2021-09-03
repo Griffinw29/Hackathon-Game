d643f85cee90f5aa81541800222e9cf4f06b500e
Elijah Wilcott <ejw773@gmail.com>
2021-09-03 17:00:15 -0400

    breaks code into components

---
+++ src/nukePositions.js
@@ -0,0 +1,24 @@
+const nukePositions = [
+    {
+      x: 750,
+      y: 200,
+    },
+    {
+      x: 1400,
+      y: 300,
+    },
+    {
+      x: 1400,
+      y: 50,
+    },
+    {
+      x: 250,
+      y: 100,
+    },
+    {
+      x: 1300,
+      y: 560,
+    }
+  ];
+
+export default nukePositions;
